810fd064e900b3354b66033983afa16884039bab
advanced-garbage <94859510+advanced-garbage@users.noreply.github.com>
2021-11-22 23:19:52 +0200

    Three exam problems from 10/23/2021
    Тук са source и project файловете от mid exam задачите по fundamentals със C#. Тук се обхващат масиви, листи и методи.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // SamsungA50, MotorolaG5, HuaweiP10
+            List<string> phones = Console.ReadLine().Split(", ").ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] tokens = input.Split();
+                string command = tokens[0];
+                string phone = string.Empty;
+                string newPhone = string.Empty;
+
+                if (command == "Bonus")
+                {
+                    string[] bonusTokens = tokens[3].Split(':');
+                    phone = bonusTokens[0]; //old phone
+                    newPhone = bonusTokens[1];
+                }
+                else
+                {
+                    phone = tokens[2];
+                }
+
+                switch(command)
+                {
+                    case "Add":
+                        if (!phones.Contains(phone))
+                        {
+                            phones.Add(phone);
+                        }
+                        break;
+                    case "Remove":
+                        if (phones.Contains(phone))
+                        {
+                            phones.Remove(phone);
+                        }
+                        break;
+                    case "Bonus":
+                        if (phones.Contains(phone))
+                        {
+                            int currIndex = phones.IndexOf(phone);
+                            if (currIndex + 1 > phones.Count - 1)
+                            {
+                                phones.Add(newPhone);
+                            }
+                            else
+                            {
+                                phones.Insert(currIndex + 1, newPhone);
+                            }
+                        }
+                        break;
+                    case "Last":
+                        if (phones.Contains(phone))
+                        {
+                            phones.Remove(phone);
+                            phones.Add(phone);
+                        }
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", phones));
+        }
+    }
+}
